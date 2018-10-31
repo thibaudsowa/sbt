39b1ed1a435d7ad4496bb0f185b9953ac91c43b2
Thibaud Sowa <thibaudsowa@gmail.com>
2018-10-31 22:13:30 +0100

    1

---
+++ src/main/java/com/thibaudsowa/sbt/SbtApplication.java
@@ -0,0 +1,12 @@
+package com.thibaudsowa.sbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtApplication.class, args);
+	}
+}
